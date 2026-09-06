08c341baf3962e0eecc2429f11620ed5bf2304e2
Amisha More <Amisha More@LAPTOP-KVPJFA46>
2026-09-06 22:02:59 +0530

    First commit

---
+++ src/main/java/com/example/demo/BankingProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProcessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingProcessServiceApplication.class, args);
+	}
+
+}
